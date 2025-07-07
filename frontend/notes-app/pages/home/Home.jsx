5fd856fa8610e8cb431fbff4e0d73c36777a936b
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2025-07-07 18:08:34 +0300

    Pages / Components

--- frontend/notes-app/pages/home/Home.jsx
+++ frontend/notes-app/pages/home/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
